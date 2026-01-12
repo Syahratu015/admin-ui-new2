606a6b4f1d2a96dd2beb2432f111e3284a0f9fab
Syahratu <111202314934@mhs.dinus.ac.id>
2026-01-13 05:20:05 +0700

    TUGAS UAS

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import svgr from "vite-plugin-svgr"; 
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(), 
-    svgr() 
-  ],
-});
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-});
->>>>>>> e74163f6df55de766972117db4a71b91c2db776f
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
+})
